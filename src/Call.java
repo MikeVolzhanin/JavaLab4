6a1e42ab040b60aa6f74fe948785d66c01dc74cd
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2024-05-18 14:07:23 +0300

    fixed bugs

--- src/Call.java
+++ src/Call.java
@@ -3,25 +3,14 @@ import java.util.Objects;
 public class Call {
     private final Direction direction;
     private final int finalLevel;
-    public Call(Direction direction, int endFloor){
+    public Call(Direction direction, int finalLevel){
         this.direction = direction;
-        this.finalLevel = endFloor;
+        this.finalLevel = finalLevel;
     }
     public Direction getDirection() {
         return direction;
     }
     public int getFinalLevel() {
         return finalLevel;
-    }
-    @Override
-    public int hashCode(){
-        return Objects.hash(finalLevel, direction);
-    }
-    @Override
-    public boolean equals(Object obj){
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Call request = (Call) obj;
-        return finalLevel == request.finalLevel && direction == request.direction;
     }
 }
